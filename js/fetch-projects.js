cdf2218167066133045b77a4b22e68c6c7d749b0
Damian Fanaro <damianfanaro@gmail.com>
2015-05-08 17:54:49 -0300

    Fetch projects JS

--- js/fetch-projects.js
+++ js/fetch-projects.js
@@ -4,7 +4,14 @@ $(document).ready(function() {
 
 	$.getJSON(userReposUrl, function(data) {
 
-		alert("sad");
+		for (var i = 0; i < data.length; i++) {
+			var item = "<a href=\"" + 
+						data[i].html_url + 
+						"\" class=\"list-group-item\" target=\"_blank\"><h5 class=\"list-group-item-heading\">" + 
+						data[i].name + 
+						"</h5></a>";
+			$("#projects-list").append(item);
+		}
 
 	});
 })
